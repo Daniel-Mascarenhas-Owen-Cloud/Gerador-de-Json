e54f04bb0f2c0d44dfb5e87b60a2ddb077c6ba4b
Daniel Mascarenhas <daniel@owencloud.com.br>
2026-07-29 12:43:30 -0300

    fix: Ajustando numero do medidor par quando for 2

--- GerarJsonMedidor.py
+++ GerarJsonMedidor.py
@@ -39,6 +39,7 @@ conteudo = (
         .replace("000.000.000.000", IP)
         .replace("Usina", usina)
         .replace("Medidor1", f"Medidor{numero_medidor}")
+        .replace("MEDIDOR1", f"Medidor{numero_medidor}")
         .replace("Cabine 1", f"Cabine {numero_medidor}")
     )
 
